7e4b389081423b36092d8dc34a9086fb7c4d89bc
Burgoos <alexanderburgossaba@gmail.com>
2022-03-19 11:01:44 -0500

    First commit

---
+++ src/main/java/com/bootcamp/PytoS2_CreditPerson/PytoS2CreditPersonApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.PytoS2_CreditPerson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PytoS2CreditPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PytoS2CreditPersonApplication.class, args);
+	}
+
+}
