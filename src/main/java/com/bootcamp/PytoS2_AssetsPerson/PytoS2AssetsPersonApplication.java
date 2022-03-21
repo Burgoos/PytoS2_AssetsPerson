4a14fc1458d0b65cbe814ec53abc1f780804b006
Burgoos <alexanderburgossaba@gmail.com>
2022-03-21 10:55:19 -0500

    Create bootsstrap.yml conection with config-server

--- src/main/java/com/bootcamp/PytoS2_AssetsPerson/PytoS2AssetsPersonApplication.java
+++ src/main/java/com/bootcamp/PytoS2_AssetsPerson/PytoS2AssetsPersonApplication.java
@@ -2,7 +2,9 @@ package com.bootcamp.PytoS2_AssetsPerson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class PytoS2AssetsPersonApplication {
 
